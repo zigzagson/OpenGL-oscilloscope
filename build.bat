dc53753065a9d6a4663efd5ec3d315c01e86a281
ZBpine <1149701807@qq.com>
2022-04-13 23:00:08 +0800

    添加了图标

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-g++ oscilloscope.cpp source/*.cpp -o oscilloscope.exe -g -m64 -Wall -static-libgcc -fexec-charset=GBK -I./include/ -L./lib/ -lglfw3dll -lglad -ljsoncpp ./lib/freetype.lib -lwsock32
+g++ oscilloscope.cpp source/*.cpp img/uestc.res -o oscilloscope.exe -g -m64 -Wall -static-libgcc -fexec-charset=GBK -I./include/ -L./lib/ -lglfw3dll -lglad -ljsoncpp ./lib/freetype.lib -lwsock32
 start oscilloscope.exe
